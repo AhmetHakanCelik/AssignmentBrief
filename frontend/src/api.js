fe28ecd2ee244c48bbfcc0e7be98adafd550b670
Ahmet272749 <ahmet.hakan.2001.ah@gmail.com>
2025-10-06 01:06:12 +0300

    Add frontend project directly

--- frontend/src/api.js
+++
@@ -1,7 +0,0 @@
-export const fetchProducts = async () => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}` / v1 / product / list);
-    if (!response.ok) {
-        throw new Error("API request failed");
-    }
-    return await response.json();
-};
