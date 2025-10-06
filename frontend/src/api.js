4d9ec40fd3a80c500ee0bffac0e90d4d10c43a40
Ahmet272749 <ahmet.hakan.2001.ah@gmail.com>
2025-10-06 05:58:23 +0300

    api.js has been updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 export const fetchProducts = async () => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}` / v1 / product / list);
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/v1/product/list`);
     if (!response.ok) {
         throw new Error("API request failed");
     }
